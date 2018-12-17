bcabc193d93cc0ed2e50f899aa25ddf714d7e946
shaoyeliudiange <shaoyeliudiange@163.com>
2018-12-17 16:23:31 +0800

    缓存视频framework

---
+++ 播放器sdk/播放器sdk/cacheVideo/DGVideoPlayerFramework.framework/Headers/DGVideoPlayerFramework.h
@@ -0,0 +1,19 @@
+//
+//  DGVideoPlayerFramework.h
+//  DGVideoPlayerFramework
+//
+//  Created by apple on 2018/12/17.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGVideoPlayerFramework.
+FOUNDATION_EXPORT double DGVideoPlayerFrameworkVersionNumber;
+
+//! Project version string for DGVideoPlayerFramework.
+FOUNDATION_EXPORT const unsigned char DGVideoPlayerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGVideoPlayerFramework/PublicHeader.h>
+#import <DGVideoPlayerFramework/DGVideoInfo.h>
+#import <DGVideoPlayerFramework/DGVideoManager.h>
